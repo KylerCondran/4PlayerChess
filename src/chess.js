273912474824267eff1af6e5f58354b173d915d6
kylercondran@gmail.com <kylercondran@gmail.com>
2025-08-10 13:25:43 -0300

    fixed board rotation again

--- src/chess.js
+++ src/chess.js
@@ -438,7 +438,7 @@ function enableDragAndDrop() {
 
 function rotateBoardForNextPlayer() {
     const chessboard = document.getElementById('chessboard');
-    boardRotation = (boardRotation - 90) % 360;
+    boardRotation = (boardRotation - 90);
     chessboard.style.transform = `rotate(${boardRotation}deg)`;
 
     // Counter-rotate all SVG pieces to stay upright
